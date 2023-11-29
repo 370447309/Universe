5838f6cac00d161e4afc103bdb9731881346901c
LJK <370447309@qq.com>
2023-11-29 11:06:33 +0800

    添加MAUI模板

---
+++ Universe/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Universe;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
